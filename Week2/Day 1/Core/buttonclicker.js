7b5e67bcef6ce98ebecdf56ce13241784dbcd50e
John Doe <johndoe@example.com>
2023-05-09 11:54:40 +0100

    alertbutton

---
+++ Week2/Day 1/Core/buttonclicker.js
@@ -0,0 +1,10 @@
+function disappear() {
+   var button = document.getElementsByClassName("def")[0];
+button.remove();
+}
+
+function logout() {
+    var button = document.getElementsByClassName("btn")[0];
+    button.innerHTML = "logout";
+    
+}
