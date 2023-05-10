759c9d718015b6e73d9236135e7e5b1172c0413d
John Doe <johndoe@example.com>
2023-05-10 15:01:11 +0100

    likes

---
+++ Week2/Day 2/Core/likes/likes.js
@@ -0,0 +1,22 @@
+
+var like1=document.querySelector("#nblikes1");
+var y=1;
+function add1(){
+    like1.innerText=y+9+"likes";
+    y++;
+    
+}
+var like2=document.querySelector("#nblikes2");
+var x=1;
+function add2(){
+    like2.innerText=x+12+"likes";
+    x++;
+    
+}
+var like3=document.querySelector("#nblikes3");
+var z=1;
+function add3(){
+    like3.innerText=z+9+"likes";
+    z++;
+    
+}
